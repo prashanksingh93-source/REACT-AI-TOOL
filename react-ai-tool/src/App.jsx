2e590866d0b6184d643e083e47a0debd18e44acc
prashanksingh93-source <prashanksingh93@gmail.com>
2026-07-13 23:11:01 +0530

    api

--- react-ai-tool/src/App.jsx
+++ react-ai-tool/src/App.jsx
@@ -1,16 +1,37 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
 
   const [question,setQuestion]=useState('');
   
+   const payload={
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": "Explain how AI works in a few words"
+          }
+        ]
+      }
+    ]}
+  const askQuestion = async () => {
+  let response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-goog-api-key": "AQ.Ab8RN6Lq0vWSggCZZ8u0f9QK_mqYYfkuVnW3hgg3xFcJFKKl7w",
+    },
+    body: JSON.stringify(payload),
+  });
 
-  const askQuestion=()=>{
-    let response=await fetch(URL)
-    console.log(question);
+  response = await response.json();
+  console.log(response);
+};
+}
     
-  }
+  
   
   return (
    <div className='grid grid-cols-5 h-screen text-center'>
@@ -29,6 +50,6 @@ function App() {
     </div> 
    </div>
   )
-}
+
 
 export default App
